ebaf83e14dd2730606010fff2cde87ef6ea06bd2
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-06-21 22:43:43 +0530

    add tenantId and code indexing for faster query

--- src/modules/coupon/couponModel.ts
+++ src/modules/coupon/couponModel.ts
@@ -1,27 +1,33 @@
 import mongoose from "mongoose";
 import { ICoupon } from "./couponTypes";
 
-const schema = new mongoose.Schema<ICoupon>({
-  title: {
-    type: String,
-    required: true,
+const schema = new mongoose.Schema<ICoupon>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+    validUpto: {
+      type: Date,
+      required: true,
+    },
+    tenantId: {
+      type: String,
+      required: true,
+    },
   },
-  code: {
-    type: String,
-    required: true,
-  },
-  discount: {
-    type: Number,
-    required: true,
-  },
-  validUpto: {
-    type: Date,
-    required: true,
-  },
-  tenantId: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true },
+);
+
+schema.index({ code: 1, tenantId: 1 }, { unique: true });
 
 export default mongoose.model("coupon", schema);
