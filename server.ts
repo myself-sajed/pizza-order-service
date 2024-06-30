587ba970414aea949b179d7846a61195cfca8b1e
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-06-30 10:11:48 +0530

    add the message consumer at the server starting level

--- server.ts
+++ server.ts
@@ -2,12 +2,19 @@ import app from "./src/app";
 import config from "config";
 import logger from "./src/config/logger";
 import connectDB from "./src/config/db";
+import { MessageBroker } from "./src/types/broker";
+import { createMessageBroker } from "./src/factories/messageBroker";
 
 const startServer = async () => {
   const PORT = config.get("server.port") || 5503;
+  let messageBroker: MessageBroker | null = null;
 
   try {
     await connectDB();
+
+    messageBroker = createMessageBroker();
+    await messageBroker.connectConsumer();
+    await messageBroker.consumeMessage(["product", "topping"], false);
 
     app
       .listen(PORT, () => console.log(`Listening on port ${PORT}`))
@@ -16,6 +23,9 @@ const startServer = async () => {
         process.exit(1);
       });
   } catch (err) {
+    if (messageBroker) {
+      await messageBroker.disconnectConsumer();
+    }
     logger.error("Error happened: ", err.message);
     process.exit(1);
   }
