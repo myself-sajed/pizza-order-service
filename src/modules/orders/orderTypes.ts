8a65cdaff684a5d909c05810d45b853d8e265e31
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-07-02 11:36:44 +0530

    add order types

---
+++ src/modules/orders/orderTypes.ts
@@ -0,0 +1,95 @@
+export interface Tenant {
+  id: string;
+  name: string;
+  address: string;
+}
+
+export type priceType = "base" | "additional";
+export type widgetType = "switch" | "radio";
+
+export interface Attributes {
+  name: string;
+  widgetType: widgetType;
+  defaultValue: string;
+  availableOptions: string[];
+}
+
+export interface AvailableOptions {
+  [key: string]: number;
+}
+
+export interface PriceConfiguration {
+  [key: string]: {
+    priceType: priceType;
+    availableOptions: AvailableOptions;
+  };
+}
+
+export interface ProductConfiguration {
+  [key: string]: string;
+}
+
+export interface Category {
+  _id: string;
+  name: string;
+  price: PriceConfiguration;
+  attributes: Attributes[];
+  hasToppings: boolean;
+}
+
+export interface Product {
+  _id: string;
+  key: string;
+  name: string;
+  isPublish: number;
+  createdAt: string;
+  description: string;
+  image: string;
+  tenantId: string;
+  categoryId: string;
+  category: Category;
+  priceConfiguration: PriceConfiguration;
+  attributes: Attributes[];
+}
+
+export interface Topping {
+  _id: string;
+  name: string;
+  price: number;
+  image: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  tenant: Tenant | null;
+}
+export interface Customer {
+  _id: string;
+  name: string;
+  email: string;
+  role: string;
+  address: Address[];
+}
+
+export interface Address {
+  address: string;
+  isDefault: boolean;
+}
+
+export interface CartItem
+  extends Pick<Product, "_id" | "name" | "image" | "tenantId"> {
+  _id: string;
+  name: string;
+  image: string;
+  tenantId: string;
+  productConfiguration: ProductConfiguration | null;
+  toppings: Topping[] | [];
+  qty: number;
+  totalPrice: number;
+}
+
+export const TAXES = 12;
+export const DELIVERY_CHARGE = 50;
