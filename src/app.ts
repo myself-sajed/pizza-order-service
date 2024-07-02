b03e043f0f65e325b83d4b2a08758c1b2427a5a5
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-07-02 11:33:57 +0530

    add order router

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
 import customerRouter from "./modules/customer/customerRouter";
 import couponRouter from "./modules/coupon/couponRouter";
+import orderRouter from "./modules/orders/orderRouter";
 import config from "config";
 import cors from "cors";
 
@@ -25,6 +26,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/customer", customerRouter);
 app.use("/coupon", couponRouter);
+app.use("/order", orderRouter);
 
 app.use(globalErrorHandler);
 
